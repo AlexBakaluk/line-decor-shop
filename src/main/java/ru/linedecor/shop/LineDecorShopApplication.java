705d6a75bde438f9e2624f82faef9e92932af6e2
AlexBakaluk <bakaluk.sasha@gmail.com>
2021-12-08 16:51:04 +0300

    initial commit

---
+++ src/main/java/ru/linedecor/shop/LineDecorShopApplication.java
@@ -0,0 +1,13 @@
+package ru.linedecor.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineDecorShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LineDecorShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/linedecor/shop/LineDecorShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.linedecor.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineDecorShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
